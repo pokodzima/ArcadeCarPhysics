4bdc101c737990605cb83c3a8b91195d4be1599d
pokodzima <52672768+pokodzima@users.noreply.github.com>
2022-10-08 22:57:49 +0600

    Add GroundNormal

--- Assets/Scripts/CarEngine.cs
+++ Assets/Scripts/CarEngine.cs
@@ -19,16 +19,41 @@ public class CarEngine : MonoBehaviour
 
     void FixedUpdate()
     {
+        Vector3 groundNormal;
+        
+        if (!isGrounded(out groundNormal)) return;
+
         if (Input.GetKey(accelerateKey))
         {
             Vector3 accelereationForce = transform.forward * acceleration;
+            accelereationForce = Vector3.ProjectOnPlane(accelereationForce, groundNormal);
             _carRigidbody.AddForce(accelereationForce);
         }
 
         if (Input.GetKey(brakeKey))
         {
             Vector3 brakingForce = -transform.forward * braking;
+            brakingForce = Vector3.ProjectOnPlane(brakingForce,groundNormal);
             _carRigidbody.AddForce(brakingForce);
+        }
+    }
+
+    private bool isGrounded(out Vector3 groundNormal)
+    {
+        Ray ray = new Ray();
+        ray.direction = -transform.up;
+        ray.origin = transform.position;
+        RaycastHit raycastHit;
+        
+        if (Physics.Raycast(ray,out raycastHit))
+        {
+            groundNormal = raycastHit.normal;
+            return true;
+        }
+        else
+        {
+            groundNormal = Vector3.zero;
+            return false;
         }
     }
 }
